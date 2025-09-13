ac9f5064174117624d144903101f6dc8e9129b14
QwQ-Op <maroofqwq@gmail.com>
2025-09-13 10:41:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.use(
 );
 
 app.post("/interactions", interactions);
-
+app.get("/", (req, res) => {
+  res.send("✅ Bot is alive!");
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
