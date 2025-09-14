9a52b5fe85669bfa7e4e347c87fa869713d8c3b1
QwQ-Op <maroofqwq@gmail.com>
2025-09-14 22:54:41 +0530

    Update interactions.js

--- api/interactions.js
+++ api/interactions.js
@@ -67,6 +67,6 @@ export default async function handler(req, res) {
     const response = await actionHandler(game, userId, data);
     return res.status(200).json(response);
   }
-
+  
   return res.status(400).send("Unhandled interaction type");
 }
